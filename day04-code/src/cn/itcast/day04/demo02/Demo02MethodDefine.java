77e34b1b1f78b5d8d5d0ff0448593edf1dbd7deb
54929 <549297913@qq.com>
2020-09-05 14:02:03 +0800

    1

--- day04-code/src/cn/itcast/day04/demo02/Demo02MethodDefine.java
+++ day04-code/src/cn/itcast/day04/demo02/Demo02MethodDefine.java
@@ -1,5 +1,13 @@
 package cn.itcast.day04.demo02;
 
 public class Demo02MethodDefine {
-    
+    public static void main(String[] args) {
+        int a = sum(10, 20);
+        System.out.println("总和是" + a);
+    }
+
+    public static int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
 }
---
+++ day04-code/src/cn/itcast/day04/demo02/Demo03MethodParam.java
@@ -0,0 +1,5 @@
+package cn.itcast.day04.demo02;
+
+public class Demo03MethodParam {
+
+}
